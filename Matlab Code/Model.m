dff5818a28ec5db539832cc5f0a81dce08ad1272
shady <shadyngheith@gmail.com>
2023-05-27 21:11:39 +0300

    send the 4 angles as JSON obj

--- Matlab Code/Model.m
+++ Matlab Code/Model.m
@@ -1,21 +1,19 @@
 clc;
 clear all;
 %%
-S = serial('COM4','BaudRate',9600,'timeOut',.05);
-fopen(S)
-arr = 10:20:80;
-fprintf(S, 'rrr');
+S = serial('COM3','BaudRate',9600,'timeOut',.05);
+fopen(S);
 pause(2);
-disp('sent dummy');
-for i = 1:length(arr)
-%     Upwm = ceil(arr(i));
-%     fprintf(S,'2');
-%     pause(0.001)
-%     fprintf(S,num2str(Upwm)); 
-    fprintf(S, strcat('t', num2str(arr(i)), 'z'));
-    pause(1);
+fprintf(S, "{""servo1"":""00.00"",""servo2"":""00.00"",""servo3"":""00.00"",""servo4"":""00.00""}");
+pause(0.5);
+while true
+data = fread(S, 1, 'uint8');
+fprintf(S, "S");
+fprintf(S, "S");
+pause(0.5);
+fprintf(S, "{""servo1"":""10.00"",""servo2"":""30.00"",""servo3"":""60.00"",""servo4"":""80.00""}");
+pause(0.5);
+disp("{""servo1"":""30.5""}");
+%pause(1);
 end
-fprintf(S, 'rrr');
-disp('sent dummy');
-pause(2);
 fclose(S);
